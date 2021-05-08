1b01e9b55e3cd60c0f14fed21dfddd7360a6f796
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-05-08 15:09:28 +0200

    empty project with test

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DummyTest {
+    @Test
+    void succeed() {
+        assertThat(true).isTrue();
+    }
+}
