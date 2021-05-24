a37b06972f76fc24fbaf022f13574218d6d9c03c
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-05-24 17:15:30 +0200

    clean up

--- src/main/java/BirthdayGreetings.java
+++ src/main/java/BirthdayGreetings.java
@@ -14,10 +14,8 @@ import java.util.Properties;
 
 public class BirthdayGreetings {
     public void send(LocalDate today) throws MessagingException, IOException {
-        // - get filepath and date as parameter
-        // - pars the file
+        // - get filepathas parameter
         // - find employee born on 2021/10/08
-        // - send email
         List<String> lines = Files.readAllLines(Path.of("employee.csv"));
         String johnDoe = lines.get(1);
         String[] johnParts = Arrays.stream(johnDoe.split(","))
@@ -26,18 +24,15 @@ public class BirthdayGreetings {
         String employeeName = johnParts[1];
         String employeeEmail = johnParts[3];
 
-        //Get the session object
         Properties properties = System.getProperties();
         properties.setProperty("mail.smtp.host", "127.0.0.1");
         properties.setProperty("mail.smtp.port", "3025");
         Session session = Session.getDefaultInstance(properties);
 
         MimeMessage msg = new MimeMessage(session);
-        //set message headers
         msg.setFrom(new InternetAddress("no-reply@foobar.com"));
         msg.setSubject("Happy birthday!");
         msg.setText("Happy birthday, dear " + employeeName + "!");
-//        msg.setText("Happy birthday, dear John!");
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(employeeEmail, false));
 
         Transport.send(msg);
