1487a9145895963a59ad17381c2479b717b28887
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-05-24 15:22:49 +0200

    disable test

--- src/test/java/BirthdayGreetingsTest.java
+++ src/test/java/BirthdayGreetingsTest.java
@@ -1,7 +1,9 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BirthdayGreetingsTest {
     @Test
+    @Disabled
     void oneGreeting() {
         //Arrange
         /* Write a file with employees
