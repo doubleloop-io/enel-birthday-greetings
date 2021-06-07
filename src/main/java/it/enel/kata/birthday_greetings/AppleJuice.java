763b7aab7a246a8024b349a4f98b88e7564b414f
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-06-07 17:24:05 +0200

    extract send mail

---
+++ src/main/java/it/enel/kata/birthday_greetings/AppleJuice.java
@@ -0,0 +1,32 @@
+package it.enel.kata.birthday_greetings;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class AppleJuice {
+    public final SmtpConfig smtpConfig;
+
+    public AppleJuice(SmtpConfig smtpConfig) {
+        this.smtpConfig = smtpConfig;
+    }
+
+    void sendMail(MailInfo mail) throws MessagingException {
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", smtpConfig.getHost());
+        properties.setProperty("mail.smtp.port", Integer.toString(smtpConfig.getPort()));
+        Session session = Session.getDefaultInstance(properties);
+
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(mail.getFrom()));
+        msg.setSubject(mail.getSubject());
+        msg.setText(mail.getBody());
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mail.getTo(), false));
+
+        Transport.send(msg);
+    }
+}
--- src/main/java/it/enel/kata/birthday_greetings/BirthdayGreetings.java
+++ src/main/java/it/enel/kata/birthday_greetings/BirthdayGreetings.java
@@ -1,27 +1,21 @@
 package it.enel.kata.birthday_greetings;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
 
 public class BirthdayGreetings {
 
     private final FileConfig fileConfig;
-    private final SmtpConfig smtpConfig;
+    private final AppleJuice appleJuice;
 
     public BirthdayGreetings(FileConfig fileConfig, SmtpConfig smtpConfig) {
         this.fileConfig = fileConfig;
-        this.smtpConfig = smtpConfig;
+        appleJuice = new AppleJuice(smtpConfig);
     }
 
     public void send(LocalDate today) throws MessagingException, IOException {
@@ -38,19 +32,7 @@ public class BirthdayGreetings {
 
             if(employee.isBirthday(today)) {
                 MailInfo mail = MailInfo.greetings(employee.getName(), employee.getEmail());
-
-                Properties properties = System.getProperties();
-                properties.setProperty("mail.smtp.host", smtpConfig.getHost());
-                properties.setProperty("mail.smtp.port", Integer.toString(smtpConfig.getPort()));
-                Session session = Session.getDefaultInstance(properties);
-
-                MimeMessage msg = new MimeMessage(session);
-                msg.setFrom(new InternetAddress(mail.getFrom()));
-                msg.setSubject(mail.getSubject());
-                msg.setText(mail.getBody());
-                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mail.getTo(), false));
-
-                Transport.send(msg);
+                appleJuice.sendMail(mail);
             }
         }
     }
