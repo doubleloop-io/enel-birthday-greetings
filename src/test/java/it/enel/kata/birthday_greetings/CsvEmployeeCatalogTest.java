d8f7b37d64d707d77133e89c81a9d04c365d52e8
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-06-13 10:05:01 +0200

    CsvEmployeeCatalog - one employee: red with puzzling error

---
+++ src/test/java/it/enel/kata/birthday_greetings/CsvEmployeeCatalogTest.java
@@ -0,0 +1,38 @@
+package it.enel.kata.birthday_greetings;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CsvEmployeeCatalogTest {
+    private FileConfig fileConfig;
+    private CsvEmployeeCatalog csvEmployeeCatalog;
+
+    @BeforeEach
+    void setUp() {
+        fileConfig = new FileConfig(Path.of("employees_test.csv"));
+        csvEmployeeCatalog = new CsvEmployeeCatalog(fileConfig);
+    }
+
+    @Test
+    void oneEmployee() throws IOException {
+        Files.write(fileConfig.getEmployeesFilePath(),
+                Arrays.asList(
+                        "last_name, first_name, date_of_birth, email",
+                        "Doe, John, 1982/10/08, john.doe@foobar.com"),
+                StandardCharsets.US_ASCII);
+
+        Employee[] employees = csvEmployeeCatalog.loadEmployees();
+
+        assertThat(employees).contains(
+                new Employee("John", "john.doe@foobar.com", new BirthDate(LocalDate.of(1982, 10, 8))));
+    }
+}
