7c981de8a3a07b62c10b672997ebf5f5adb9d447
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-06-13 09:41:38 +0200

    add CsvEmployeeCatalog

---
+++ src/main/java/it/enel/kata/birthday_greetings/CsvEmployeeCatalog.java
@@ -0,0 +1,9 @@
+package it.enel.kata.birthday_greetings;
+
+public class CsvEmployeeCatalog {
+    public final FileConfig fileConfig;
+
+    public CsvEmployeeCatalog(FileConfig fileConfig) {
+        this.fileConfig = fileConfig;
+    }
+}
