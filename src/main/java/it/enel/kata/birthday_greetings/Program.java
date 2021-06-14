d402612d60d20efbeac8a2664e654d10bed5e300
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-06-14 16:26:32 +0200

    clean up

--- src/main/java/it/enel/kata/birthday_greetings/Program.java
+++ src/main/java/it/enel/kata/birthday_greetings/Program.java
@@ -6,12 +6,11 @@ import it.enel.kata.birthday_greetings.infrastructure.FileConfig;
 import it.enel.kata.birthday_greetings.infrastructure.SmtpConfig;
 import it.enel.kata.birthday_greetings.infrastructure.SmtpMailSender;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.time.LocalDate;
 
 public class Program {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         CsvEmployeeCatalog csvEmployeeCatalog = new CsvEmployeeCatalog(new FileConfig(Path.of("empty_employees.csv")));
         SmtpMailSender smtpMailSender = new SmtpMailSender(new SmtpConfig("127.0.0.1", 1025));
         BirthdayGreetings birthdayGreetings = new BirthdayGreetings(csvEmployeeCatalog, smtpMailSender);
